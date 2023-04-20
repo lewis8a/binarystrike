51c6892197b7a9fb5cbb10e3b1f9229cba03128b
Lewis <lewis8a@gmail.com>
2023-04-20 19:00:00 -0400

    More sprites (Objects) and Test Tileset2

--- tilemaps/tileset.tsx
+++ tilemaps/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="Tileset" tilewidth="16" tileheight="16" tilecount="720" columns="36">
- <image source="../assets/graphics/Tileset.png" width="576" height="320"/>
+<tileset version="1.2" tiledversion="1.3.2" name="Tileset" tilewidth="16" tileheight="16" tilecount="720" columns="36">
+ <image source="../assets/graphics/tilesets/Tileset.png" width="576" height="320"/>
 </tileset>
