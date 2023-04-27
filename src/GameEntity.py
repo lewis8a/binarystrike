f3c47088cffe80a3865806599a8681fa30c63f4a
Kevin <marquezberriosk@gmail.com>
2023-04-27 17:10:43 -0400

    Arreglo de bug al caminar

--- src/GameEntity.py
+++ src/GameEntity.py
@@ -149,7 +149,7 @@ class GameEntity(mixins.DrawableMixin, mixins.AnimatedMixin, mixins.CollidableMi
         collision_rect = self.get_collision_rect()
 
         # Row for the center of the player
-        i = self.tilemap.to_i(collision_rect.centery)
+        i = self.tilemap.to_i(collision_rect.bottom - 8)
         # Left and right columns
         left = self.tilemap.to_j(collision_rect.left)
         right = self.tilemap.to_j(collision_rect.right)
