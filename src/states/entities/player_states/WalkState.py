3dc6155e69607a12358349228ed07b999af1aefa
Kevin <marquezberriosk@gmail.com>
2023-05-04 18:33:03 -0400

    Procesado de mirar hacia arriba mientras camina

--- src/states/entities/player_states/WalkState.py
+++ src/states/entities/player_states/WalkState.py
@@ -39,13 +39,17 @@ class WalkState(BaseEntityState):
                 self.entity.flipped = True
             elif input_data.released and self.entity.vx <= 0:
                 self.entity.change_state("idle")
-
         elif input_id == "move_right":
             if input_data.pressed:
                 self.entity.vx = settings.PLAYER_SPEED
                 self.entity.flipped = False
             elif input_data.released and self.entity.vx >= 0:
                 self.entity.change_state("idle")
+        elif input_id == "look_up":
+            if input_data.pressed:
+                self.entity.change_animation("walk-up")
+            else:
+                self.entity.change_animation("walk")
         elif input_id == "jump" and input_data.pressed:
             self.entity.double_jump = False
             self.entity.change_state("jump")
